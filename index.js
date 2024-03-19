13a7fa62f6444911f9df1c79b83d11204d037916
biancaalves27 <142455130+biancaalves27@users.noreply.github.com>
2024-03-19 16:18:57 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+document.writeln('Hello World'); 
+console.log('Hello, World'); 
+const hello3Div = document.querySelector("#hello3");
+  
+
+hello3Div.textContent = "Hello, world!";
+
+alert('Hello World'); 
+
+document.write("Hello World!");
+
+const hello = ["hello world"];
+document.getElementById("hello").innerHTML = hello;
+
+function myFunction() {
+document.getElementById("world").innerHTML = "Hello World!";
+}
+
+function multiply(hello, world) {
+    let result = hello * world;
+    return result;
+  }
